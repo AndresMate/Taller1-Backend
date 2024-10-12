d56bc173d6544ca278d2ac7972019f2d71921de8
AndresMate <andres.mateus01@uptc.edu.co>
2024-10-12 16:21:33 -0500

    Update controll-dealer.js

--- controllers/controll-dealer.js
+++ controllers/controll-dealer.js
@@ -5,35 +5,35 @@ module.exports = {
         try {
             const { id, name, area } = req.body;
             const dealer = await Dealer.create({ id, name, area });
-            return res.status(201).send(dealer);
+            return res.status(201).json(dealer);
         } catch (error) {
-            return res.status(400).send({ error: 'Error creating new dealer' });
+            return res.status(400).json({ error: 'Error creating new dealer' });
         }
     },
     'listDealer': async (req, res) => {
         try {
             const dealer = await Dealer.find();
-            return res.status(200).json({state:true, data: dealer})
+            return res.status(200).json({ state: true, data: dealer });
         } catch (error) {
-            return res.status(500).json({state:false, message:error});
+            return res.status(500).json({ state: false, message: error });
         }
     },
     'updateDealer': async (req, res) => {
         const { id, name, area } = req.body;
         try {
             const dealer = await Dealer.findOneAndUpdate({ id }, { name, area }, { new: true });
-            return res.status(200).send(dealer);
+            return res.status(200).json(dealer);
         } catch (error) {
-            return res.status(400).send({ error: 'Error updating dealer' });
+            return res.status(400).json({ error: 'Error updating dealer' });
         }
     },
     'deleteDealer': async (req, res) => {
         const { id } = req.body;
         try {
             await Dealer.findOneAndDelete({ id });
-            return res.status(200).send({ message: 'Dealer deleted' });
+            return res.status(200).json({ message: 'Dealer deleted' });
         } catch (error) {
-            return res.status(400).send({ error: 'Error deleting dealer' });
+            return res.status(400).json({ error: 'Error deleting dealer' });
         }
     },
     'findById': async (req, res) => {
